d8400adf7def572cc7cd7da8bf29a66c1c02de07
Stefan Wanigatunga <stefan.wanigatunga@gmail.com>
2024-06-26 16:40:42 -0700

    adds routes and models

---
+++ routes/api/category-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  const categories = await Category.findAll({
+    include: [Product]
+  })
+  res.json(categories)
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  const category = await Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  res.json(category)
+});
+
+router.post('/', async (req, res) => {
+  // create a new category
+  const category = await Category.create(req.body)
+  res.json(category)
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  const category = await Category.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+  res.json(category)
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+const category = await Category.destroy({
+  where: {
+    id: req.params.id
+  },
+})
+res.json(category)
+});
+
+module.exports = router;
